89f3120bfb4cc81e6f24e83a91fb9a8f25b99b10
Charles Rymal <charles.rymal@nortek.com>
2018-04-18 20:11:43 -0700

    clean up test function

--- jessica/tests/test_render.py
+++ jessica/tests/test_render.py
@@ -37,21 +37,7 @@ async def test_render():
 @pytest.mark.asyncio
 async def test_render_mongo():
 
-        #d1 = os.path.join(d, 'source')
-
-        #os.makedirs(d1)
-        #os.makedirs(os.path.join(d1, 'templates'))
-
-        #with open(os.path.join(d1, 'index.md'), 'w') as f:
-        #    f.write('{% set template = "temp1.html" %}\n# hello\n\n[test](a/b/c)')
-
-        #with open(os.path.join(d1, 'templates', 'default.html'), 'w') as f:
-        #    f.write('{% block body %}{% endblock %}')
-
-        #with open(os.path.join(d1, 'templates', 'temp1.html'), 'w') as f:
-        #    f.write('<html><head></head><body>{% block body %}{% endblock %}</body></html>')
-
-        e = jessica.Engine(jessica.SourceMongo(f'test_{int(time.time())}'))
+        e = jessica.EngineDB(f'test_{int(time.time())}')
 
         text = '{% set template = "temp1.html" %}\n# hello\n\n[test](a/b/c)'
 
