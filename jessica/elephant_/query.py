15010e0b8354e5df5d7f3b06bb36559655b5e82f
Charles Rymal <charlesrymal@gmail.com>
2018-05-31 23:06:23 +0000

    remove elephant untracked

--- jessica/elephant_/query.py
+++ jessica/elephant_/query.py
@@ -1,12 +1,12 @@
-import elephant.file
+import elephant.local_
 
-class Query(elephant.file._AArray):
+class Query(elephant.local_.File):
     def to_array(self):
         d = dict(self.d)
-        d["_collection"] = "tasks queries"
+        d["_collection"] = "texts queries"
         return d
 
-class Engine(elephant.file.Engine):
+class Engine(elephant.local_.Engine):
     def _factory(self, d):
         return Query(self.coll, d)
 
